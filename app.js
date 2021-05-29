e89e32747c305659e5dd4c44014bce28751e3740
kemolinari <72270671+kemolinari@users.noreply.github.com>
2021-05-29 18:11:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 
 
-$(".navbar-toggler-icon").click(function(){
-
-  $(".background-title").fadeToggle();
-
-$(".grafic").slideToggle();
-
-$(".grafic2").slideToggle();
-
-});
-
 
 $("*").click(function(){
-
-  $(".background-title").show();
-
-  $(".grafic").show();
-
-  $(".grafic2").show();
 
   $(".navbar-collapse").removeClass("show");
 
